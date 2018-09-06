9679dc6277e4a018a92096b23e1c3d89c248e4d8
Håkon <hakon.strom.lie@gmail.com>
2018-09-07 00:32:02 +0200

    More changes to comment definitions

--- src/main/java/no/hakon/httpRequest/HttpResponse.java
+++ src/main/java/no/hakon/httpRequest/HttpResponse.java
@@ -8,7 +8,9 @@ public class HttpResponse {
 	HashMap<String, String> headers;
 	
 	// Constructor opens HashMap and temporary String array for converting headers. Will store header parameter as key (without colon), and
-	// store header content as value(including everything after first space). Stops reading at "Connection: close" to avoid nullpointerexception
+	// store header content as value(including everything after first space). Stops reading at "Connection: close" to avoid nullpointerexception.
+	// @param response is String separating lines by "\r\n".
+	
 	public HttpResponse(String response) 
 	{	
 		headers = new HashMap<String, String>();
@@ -22,14 +24,15 @@ public class HttpResponse {
 
 	}
 
-	// use getHeader to fetch statusCode by searching and splitting the code from the message
-	// Error might show if there is no " " to split on? Not sure
-		public int getStatusCode() 
-		{			
-			return Integer.parseInt(getHeader("HTTP/1.1").split(" ")[0]);
+	// Will fetch statusCode by searching getHeader and splitting the actual code from the message.
+	// Error might show if there is no " " to split on? Not sure.
+	public int getStatusCode() 
+	{			
+		return Integer.parseInt(getHeader("HTTP/1.1").split(" ")[0]);
 	}
 	 
 	// getHeader check if input is valid and then searches the HashMap for the key and returns the corresponding value		
+	// @param headerName must be identical to Key in HashMap as withdrawn from response.
 	public String getHeader(String headerName) 
 	{		
 		if(headerName == null || headerName == "") return null;		
